ae4410e7132d6f462b53426ffabb4fb75201f68f
mureithi254 <lnewhenio@gmail.com>
2017-02-24 15:25:08 +0300

    routing&gallery

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
-
+  root 'home#index'
   get 'home/about'
-
   get 'home/gallery'
-
   get 'home/contact'
-
   get 'home/services'
 
-  root 'application#hello'
+  # root 'application#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
